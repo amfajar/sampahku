c426e72b9ed5add514a3dfabf4d79dfed9e1bdf6
amfajar <amfajar222@gmail.com>
2022-06-03 01:02:42 +0700

    Tambah library

--- testing/test.py
+++ testing/test.py
@@ -2,6 +2,6 @@ import requests
 x = 10
 while x > 0:
     
-    resp = requests.post("http://localhost:5000", files={"image": open("rokok.jpeg", "rb")})
+    resp = requests.post("https://sampahku-ml-api-r5feak6qfq-et.a.run.app:8080", files={"image": open("./rokok.jpeg", "rb")})
     print(resp.json())
     x -= 1
